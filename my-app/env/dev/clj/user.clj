ce9bda7d7bfdae8590afa31f871cd5a9ea0f6dca
Alberto Biasão <abiasao@br.ibm.com>
2017-11-24 10:40:32 -0200

    adding friboo and liberator examples

---
+++ my-app/env/dev/clj/user.clj
@@ -0,0 +1,16 @@
+(ns user
+  (:require 
+            [mount.core :as mount]
+            my-app.core))
+
+(defn start []
+  (mount/start-without #'my-app.core/repl-server))
+
+(defn stop []
+  (mount/stop-except #'my-app.core/repl-server))
+
+(defn restart []
+  (stop)
+  (start))
+
+
